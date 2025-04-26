b598979d00291b2354cdff37ed13be68318dfd01
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-04-27 00:12:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import soundfile as sf
 from transformers import Qwen2_5OmniForConditionalGeneration, Qwen2_5OmniProcessor
 from qwen_omni_utils import process_mm_info
+import io
+import base64
+import inferless
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+@inferless.request
+class RequestObjects(BaseModel):
+    prompt: str = Field(default="image")
+    system_prompt: str = Field(default="image")
+    return_audio: bool = Field(default=True)
+    video_url: Optional[str] = None
+    image_url: Optional[str] = None
+    audio_url: Optional[str] = None
+    audio_url: Optional[str] = None
+
+@inferless.response
+class ResponseObjects(BaseModel):
+    generated_text: str = Field(default="Test output")
+    generated_audio: Optional[str] = None
 
 
 class InferlessPythonModel:
@@ -10,14 +31,14 @@ class InferlessPythonModel:
         self.processor = Qwen2_5OmniProcessor.from_pretrained(model_id)
         
 
-    def infer(self, inputs):
-        prompt = inputs.get("prompt", "What are the elements can you see and hear in these medias?")
-        system_prompt = inputs.get("prompt", "You are Qwen, a virtual human developed by the Qwen Team, Alibaba Group, capable of perceiving auditory and visual inputs, as well as generating text and speech.")
-        video_url = inputs.get("video_url",None)
-        image_url = inputs.get("image_url",None)
-        audio_url=inputs.get("audio_url",None)
+    def infer(self, request: RequestObjects) -> ResponseObjects:
+        # prompt = inputs.get("prompt", "What are the elements can you see and hear in these medias?")
+        # system_prompt = inputs.get("prompt", "You are Qwen, a virtual human developed by the Qwen Team, Alibaba Group, capable of perceiving auditory and visual inputs, as well as generating text and speech.")
+        # video_url = inputs.get("video_url",None)
+        # image_url = inputs.get("image_url",None)
+        # audio_url=inputs.get("audio_url",None)
         
-        conversation = self.get_query(system_prompt,  prompt, video_url, image_url, audio_url)
+        conversation = self.get_query(request.system_prompt,  request.prompt, request.video_url, request.image_url, request.audio_url)
 
         text = self.processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
         audios, images, videos = process_mm_info(conversation, use_audio_in_video=True)
@@ -28,11 +49,21 @@ class InferlessPythonModel:
         text_ids, audio = self.model.generate(**inputs, use_audio_in_video=True)
         text = self.processor.batch_decode(text_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
         print(text)
+
+        buffer = io.BytesIO()
         sf.write(
-            "output.wav",
+            buffer,
             audio.reshape(-1).detach().cpu().numpy(),
             samplerate=24000,
+            format='WAV'
         )
+        
+        buffer.seek(0)
+        audio_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+        print(audio_base64[:150])
+
+        generateObject = ResponseObjects(generated_text=text,generated_audio=audio_base64)        
+        return generateObject
     
     def get_query(self, system_prompt,  prompt, video_url=None, image_url=None, audio_url=None):
         content_list = []
